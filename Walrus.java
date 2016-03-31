ffecd953e49f87a5c863ce0233b4a6abbd4168bf
ryansutc <ryansutc@gmail.com>
2016-03-31 20:46:15 -0300

    Update Walrus.java
    First working version supporting multiple GPS points (Major update)

--- Walrus.java
+++ Walrus.java
@@ -2,20 +2,27 @@ package penguins;
 
 public class Walrus extends Animal {
 	private String dentalhealth;
-	public Walrus(String species, char sex, float weight, double[][] gps) {
-		super(species, sex, weight, gps);
+	public Walrus(String species, char sex, float weight, String coords) {
+		super(species, sex, weight, coords);
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Walrus(String species, char sex, float weight, double[][] gps, String dentalhealth) {
+	public Walrus(String species, char sex, float weight, String coords, String dentalhealth) {
+		super(species, sex, weight, coords);
+		// TODO Auto-generated constructor stub
+		this.setDentalhealth(dentalhealth);
+	}
+	
+	public Walrus(String species, char sex, float weight, Gps gps, String dentalhealth) {
 		super(species, sex, weight, gps);
 		// TODO Auto-generated constructor stub
 		this.setDentalhealth(dentalhealth);
 	}
 	
+	
 	@Override
 	public String makeString(){
-		String s = super.makeString() + "\t" + this.getDentalhealth();
+		String s = super.makeString() + "   " + this.getDentalhealth();
 		return s;
 	}
 	
